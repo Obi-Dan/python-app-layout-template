c2b1ce9002109b4a511630058839d14f8d3e5a9e
Daniel Hofferbert <>
2022-11-28 11:18:24 -0600

    Finished python layout template

--- src/tests/test_runner.py
+++ src/tests/test_runner.py
@@ -1,10 +1,10 @@
-from unittest.mock import patch, call
+from unittest.mock import patch, call, MagicMock
 
 from src import runner
 
 
 @patch('builtins.print')
-def test_hello_world(mock_print) -> None:
+def test_hello_world(mock_print: MagicMock) -> None:
     runner.main()
 
     assert mock_print.mock_calls == [
